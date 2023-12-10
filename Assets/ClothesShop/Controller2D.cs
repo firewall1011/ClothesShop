2396582f30d5052ecb1e93fd152605e7f9767b48
Tiago Marino <tmsilva11@gmail.com>
2023-12-10 02:46:51 -0300

    Add Player Controller 2D

---
+++ Assets/ClothesShop/Controller2D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ClothesShop
+{
+    public abstract class Controller2D : MonoBehaviour
+    {
+        /// <summary>
+        /// Returns normalized movement direction
+        /// </summary>
+        /// <returns></returns>
+        public abstract Vector2 GetMoveDirection();
+    }
+}
---
+++ Assets/ClothesShop/PlayerController2D.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using ClothesShop.Input;
+
+namespace ClothesShop
+{
+    public class PlayerController2D : Controller2D, PlayerInputActions.IInWorldActions
+    {
+        private Vector2 _currentMoveDirection;
+        private PlayerInputActions _playerInputActions;
+        private void Awake()
+        {
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.InWorld.SetCallbacks(this);
+        }
+        
+        private void OnEnable() => _playerInputActions.InWorld.Enable();
+        private void OnDisable() => _playerInputActions.InWorld.Disable();
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _currentMoveDirection = context.ReadValue<Vector2>();
+        }
+
+        public override Vector2 GetMoveDirection() => _currentMoveDirection;
+    }
+}
