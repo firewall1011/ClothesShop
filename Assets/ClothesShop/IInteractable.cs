db8ccfccd6a42659329a710874e1529754223cb7
Tiago Marino <tmsilva11@gmail.com>
2023-12-11 01:53:29 -0300

    Refactor InteractionComponent to receive Instigator

--- Assets/ClothesShop/IInteractable.cs
+++ Assets/ClothesShop/IInteractable.cs
@@ -1,7 +1,9 @@
-﻿namespace ClothesShop.Character
+﻿using UnityEngine;
+
+namespace ClothesShop
 {
     public interface IInteractable
     {
-        void Interact();
+        void Interact(GameObject instigator);
     }
 }
